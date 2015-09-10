596fc2ca7395d4497122072bc90b4e6113136f31
wil <wil@crofter.net>
2015-09-10 15:29:57 -0400

    bug fixes

--- R/smooth.R
+++ R/smooth.R
@@ -1,8 +1,20 @@
 library(Matrix)
 
 # Determine S matrix column associated with pixel i,j
-ij2col <- function(i, j, nr, nc){
-  (j-1)*nr + i
+#
+# pixrow is the pixel ROW in the IMAGE as it appears in a call to image,
+# numbered from bottom up. It corresponds to the COLUMN of the
+# image regarded as a MATRIX. In other words, ipix varies in the
+# vertical dimension of the image from bottom up.
+#
+# pixcol is the pixel COLUMN (matrix row), and varies from right
+# to left in the image.
+# 
+# height is the HEIGHT of the IMAGE, the number of pixel rows.
+# width is , the WIDTH of the image, the number of pixel columns.
+ij2col <- function(pixrow, pixcol, height, width){
+  # (j-1)*nr + i
+  (pixrow-1)*width + pixcol
 }
 
 # Determine the S matrix row associated with transducer pair i, j
@@ -38,18 +50,19 @@ formV <- function(nr, nc){
   t(V) %*% V
 }
 
-# NOTE: n is the number of rows of pixels in the IMAGE, m the number of columns.
-# BUT, n is the number of COLUMNS in the corresponding matrix.
-genS <- function(n, m, gridsize){
-  S <- Matrix(0, n^2, n*m)
-  for(i in 1:n){
-    for(j in 1:n){
+# NOTE: height is the number of rows of pixels in the IMAGE, width the number of columns.
+genS <- function(height, width, gridsize){
+  S <- Matrix(0, height^2, height*width)
+  for(i in 1:height){
+    for(j in 1:height){
       u <- c(-1e-6, (i-0.5)*gridsize, 0)
-      v <- c(m*gridsize+1e-6, (j-0.5)*gridsize, 0)
-      segs <- segmentLengths(m, n, u, v, gridsize)
-      ridx <- ij2row(i,j,n)
+      v <- c(width*gridsize+1e-6, (j-0.5)*gridsize, 0)
+      segs <- segmentLengths(width, height, u, v, gridsize)
+      ridx <- ij2row(i,j,height)
       for(k in 1:nrow(segs)){
-        S[ridx, ij2col(segs[k,2], segs[k,1], n, m)] <- segs[k,3]
+        # segs[k,2] is the IMAGE Y dimension (height) which corresponds to pixel ROW
+        # segs[k,1] is the IMAGE X dimension (width) which corresponds to pixel COL
+        S[ridx, ij2col(segs[k,2], segs[k,1], height, width)] <- segs[k,3]
       }
     }
   }
