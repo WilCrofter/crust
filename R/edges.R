5fddb564f335f189f2212c43e90dd4f77f33f281
wil <wil@crofter.net>
2015-10-03 16:50:11 -0400

    piecewise dimension reduction in progress

---
+++ R/edges.R
@@ -0,0 +1,52 @@
+edges <- list(m1=matrix(1,4,4), m2=matrix(c(1,1,-1,-1), 4, 4), m3=t(matrix(c(1,1,-1,-1),4,4)))
+M <- sapply(edges, as.vector)
+ihat <- matrix(0,365,61)
+for(i in seq(1, 361, by=4)){
+  for(j in seq(1, 57, by=4)){
+    e <- img[i:(i+3), j:(j+3)]
+    ihat[i:(i+3), j:(j+3)] <- matrix(M %*% solve(t(M) %*% M, t(M) %*% as.vector(e)), 4, 4)
+  }
+}
+# image(x=(1:364)/2, y=(1:60)/2, z=ihat[1:364,1:60], asp =1)
+
+changeOfVariable <- function(edges, height, width){
+  M <- sapply(edges, as.vector)
+  p <- solve(t(M) %*% M) %*% t(M)
+  by <- dim(edges[[1]])[1]
+  P <- matrix(0, length(edges)*ceiling(height/by)*ceiling(width/by), height*width)
+  rowP <- 0
+  tbl <- cbind(rep(1:by,by),rep(1:by,each=by))
+  for(j in seq(1, height, by=by)){
+    for(i in seq(1, width, by=by)){
+      if((tbl[dim(p)[2],1] + i - 1 > width) | (tbl[dim(p)[2],2]+j-1 > height))continue
+      for(k in 1:(dim(p)[1])){
+        rowP <- rowP + 1
+        for(m in 1:(dim(p)[2])){
+          iCol <- tbl[m,1] + i - 1
+          jCol <- tbl[m,2] + j - 1
+          colP <- (jCol-1)*width + iCol
+          P[rowP, colP] <- p[k,m]
+        }
+      }
+    }
+  }
+  P
+}
+
+recoverImg <- function(coefs, edges, height, width){
+  M <- sapply(edges, as.vector)
+  by <- dim(edges[[1]])[1]
+  img <- matrix(0, width, height)
+  patches <- M %*% matrix(coefs, ncol(M), length(coefs)/ncol(M))
+  patchIdx <- 0
+  for(j in seq(1, height, by=by)){
+    for(i in seq(1,width, by=by)){
+      if((i+by-1) > width | (j+by-1) > height ){
+        print("hi")
+      }
+      patchIdx <- patchIdx + 1
+      img[i:(i+by-1), j:(j+by-1)] <- matrix(patches[,patchIdx], by, by)
+    }
+  }
+  img
+}
