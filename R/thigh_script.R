ef15398a96505ec5e1f81664f8c6e3a765c36729
wil <wil@crofter.net>
2015-10-29 15:38:04 -0400

    pixelated test suite in progress

---
+++ R/thigh_script.R
@@ -0,0 +1,43 @@
+if(!("R" %in% dir()))stop("This file must be sourced from the project directory.")
+source("R/inkscape_utils.R")
+source("C/ctest.R")
+
+# Utility to calculate gridsize in mm which will give a certain height, h, in pixels
+# for a given rectangl, r.
+calcGridSize <- function(r, h){
+  hv <- r[4,] - r[1,]
+  lhv <- sqrt(sum(hv^2))
+  mean(c(lhv/(h-.5), lhv/(h-1.5)))
+}
+
+generatePixImages <- function(){
+  phantom <- importIPhantom("data/thigh_inkscape.csv")
+  ap <- read.csv("data/acoustic_properties_thigh.csv", as.is=TRUE)
+  scan_1 <- computeRectangle(phantom, c(100,100), pi/4, 127*.23)
+  scan_2 <- computeRectangle(phantom, c(100,100), pi/4 + pi/8, 127*.23)
+  scan_3 <- computeRectangle(phantom, c(90, 80), 0, 127*.23)
+  scan_4 <- computeRectangle(phantom, c(180, 125), pi/8, 127*.23)
+  scan_5 <- computeRectangle(phantom, c(125,140), pi/2+pi/4+pi/16, 127*.23)
+  scan_6 <- computeRectangle(phantom, c(130,125), pi/2+pi/4-pi/8, 127*.23)
+  scan_7 <- computeRectangle(phantom, c(130,125), pi/2-pi/16, 127*.23)
+  scan_8 <- computeRectangle(phantom, c(180,125), pi/4+pi/16, 127*.23)
+  scans <- paste0("scan_",1:8)
+  for(n in 1:8){
+    scan <- get(scans[n])
+    for(res in 2^(4:7)){
+      gridsize <- calcGridSize(scan, res)
+      img <- pixelate(phantom, scan, gridsize)
+      colnames(img) <- as.character(signif(gridsize*(1:ncol(img)-1),4))
+      outf <- paste0("data/",scans[n],"_",res,".csv")
+      write.csv(as.data.frame(img),outf, row.names=FALSE)
+      writeLines(outf)
+    }
+  }
+}
+
+readScan <- function(scan, resolution){
+  temp <- as.matrix(read.csv(paste0("data/scan_",scan,"_",resolution,".csv"), 
+                             as.is=TRUE, comment.char = "#"))
+  colnames(temp) <- gsub("X","",colnames(temp))
+  temp
+}
