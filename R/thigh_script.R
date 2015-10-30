67086505ac374225acd50609923e176c18876987
wil <wil@crofter.net>
2015-10-30 17:34:39 -0400

    test suite completed

--- R/thigh_script.R
+++ R/thigh_script.R
@@ -1,17 +1,15 @@
-if(!("R" %in% dir()))stop("This file must be sourced from the project directory.")
-source("R/inkscape_utils.R")
-source("C/ctest.R")
 
 # Utility to calculate gridsize in mm which will give a certain height, h, in pixels
-# for a given rectangl, r.
+# for a given rectangle, r.
 calcGridSize <- function(r, h){
   hv <- r[4,] - r[1,]
   lhv <- sqrt(sum(hv^2))
   mean(c(lhv/(h-.5), lhv/(h-1.5)))
 }
 
-generatePixImages <- function(){
-  phantom <- importIPhantom("data/thigh_inkscape.csv")
+generatePixImages <- function(datadir="data"){
+  source(paste0(datadir, "../R/inkscape_utils.R"))
+  phantom <- importIPhantom(paste0(datadir,"/thigh_inkscape.csv"))
   ap <- read.csv("data/acoustic_properties_thigh.csv", as.is=TRUE)
   scan_1 <- computeRectangle(phantom, c(100,100), pi/4, 127*.23)
   scan_2 <- computeRectangle(phantom, c(100,100), pi/4 + pi/8, 127*.23)
@@ -28,16 +26,26 @@ generatePixImages <- function(){
       gridsize <- calcGridSize(scan, res)
       img <- pixelate(phantom, scan, gridsize)
       colnames(img) <- as.character(signif(gridsize*(1:ncol(img)-1),4))
-      outf <- paste0("data/",scans[n],"_",res,".csv")
+      outf <- paste0(datadir,"/",scans[n],"_",res,".csv")
       write.csv(as.data.frame(img),outf, row.names=FALSE)
       writeLines(outf)
     }
   }
 }
 
-readScan <- function(scan, resolution){
-  temp <- as.matrix(read.csv(paste0("data/scan_",scan,"_",resolution,".csv"), 
+readScan <- function(scan, resolution, datadir="data"){
+  temp <- as.matrix(read.csv(paste0(datadir,"/scan_",scan,"_",resolution,".csv"), 
                              as.is=TRUE, comment.char = "#"))
   colnames(temp) <- gsub("X","",colnames(temp))
   temp
+}
+
+tissue2Slowness <- function(img, datadir="data"){
+  ap <- read.csv(paste0(datadir,"/acoustic_properties_thigh.csv"), 
+                        as.is=TRUE, comment.char = "#")
+  for(id in unique(as.vector(img))){
+    idx <- img==id 
+    img[idx] <- ap[ap$ID==id, "speed"]
+  }
+  1/img
 }
