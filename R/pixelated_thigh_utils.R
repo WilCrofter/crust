bbbb16eda87d5d5e84183573b996a237ad05ea10
wil <wil@crofter.net>
2015-12-08 22:03:10 -0500

    utilities to find minimal rectangle enclosing a portion of thigh in progress

---
+++ R/pixelated_thigh_utils.R
@@ -0,0 +1,102 @@
+# TODO: Use C versions of, e.g., wCrossings or perhaps port these functions to C.
+
+# Given a grid size, convert from x,y coordinates, c(x,y), to row-column coordinates
+# assuming the pixel, img[1,1], has corners c(0,0) and c(gridsize, gridsize)
+xy2pixel <- function(xycoords, gridsize){
+  # The floor and ceiling of an integer are equal, hence the use of floor + 1
+  1+floor(xycoords/gridsize)
+}
+
+# Given grid size and the indices, c(i,j), of a pixel, return the x,y
+# coordinates of its midpoint.
+pixel2xy <- function(ijpix, gridsize){
+  (ijpix-0.5)*gridsize
+}
+
+# Find the points of a grid crossed by a line segment whose two endpoints are u and v
+# both of which are 2-tuples. The pixel grid is defined by a gridsize and
+# assumed parallel to the x and y axes. Crossings will be in order from u to v.
+gridCrossings <- function(u, v, gridsize, img, eps = (.Machine$double.eps)^.75){
+  kx <- seq(0, 1+nrow(img))*gridsize
+  ky <- seq(0, 1+ncol(img))*gridsize
+  # wCrossings requires 3 vectors:
+  lambdas <- sort(unique(c(wCrossings(c(u,0), c(v,0), c(1,0,0), kx), 
+                           wCrossings(c(u,0), c(v,0), c(0,1,0), ky))))
+  # Since lambdas are sorted in ascending order, crossings well be
+  # in order from u to v. Eliminate any which are too close together.
+  idx <- which(diff(lambdas) < eps)
+  if(length(idx)>0)lambdas <- lambdas[-(1+idx)]
+  crossings <- sapply(lambdas, function(lambda)u+lambda*(v-u))
+}
+
+# Given coordinates of grid crossings, find the pixels crossed.
+pixelsCrossed <- function(crossings, gridsize){
+  # The average of two consecutive crossings should be in the interior
+  # of the pixel which they bound
+  midpoints <- (crossings[,-ncol(crossings)] + crossings[,-1])/2
+  apply(midpoints, 2, function(u)xy2pixel(u,gridsize))
+}
+
+# Given a 2xn array of pixel indices, return only the indices
+# which satisfy the given function
+pixelsSatisfying <- function(indices, img, fct){
+  indices[,which(apply(indices, 2, function(idx)fct(img[idx[1], idx[2]])))]
+}
+
+# Given a 2xn array of pixel indices, cull those out of bounds with respect
+# to the dimensions of img
+cullIndices <- function(indices, img){
+  idx <- indices[1,] > 0 & indices[1,] <= nrow(img) & 
+    indices[2,] > 0 & indices[2, ] <= ncol(img)
+  indices[,idx]
+}
+
+# Given two endpoints, u and v, of a line segment, return the indices of the
+# nonzero pixels which it crosses.
+nonzeroPix <- function(u, v, img, gridsize){
+  # Grid crossings will be returned in order from u to v
+  crossings <- gridCrossings(u, v, gridsize, img)
+  # In-bound pixels crossed by the line in order from u to v
+  pix <- cullIndices(pixelsCrossed(crossings, gridsize), img)
+  # Of these, the pixels with nonzero entries
+  pixelsSatisfying(pix, img, function(x)x!=0)
+}
+
+# Find a minimal rectangle of specified position, orientation, and height which encloses
+# a portion of the non-zero pixels in an image. The gridsize, center coordinates, and 
+# height must be in the same units, usually mm.
+minRect <- function(img, gridsize, center, angle_in_radians, height){
+  # Equation of the line
+  m <- tan(angle_in_radians)
+  b <- center[2]-m*center[1]
+  # Endpoints within the grid
+  u <- c(0, b)
+  xmax <- nrow(img)*gridsize
+  v <- c(xmax, m*xmax+b)
+  # Indices of nonzero pixels crossed by the segment
+  nzpix <- nonzeroPix(u, v, img, gridsize)
+  # That the line segment should intersect tissue is a programming convenience.
+  # It's not strictly necessary, but would require more code than it's worth.
+  if(length(nzpix) == 0)stop("Given line segment does not intersect tissue.")
+  # The endpoints of nzpix approximately bound the tissue.
+  aprxbounds <- nzpix[,c(1,ncol(nzpix))]
+  # Find their midpoints in x,y coordinates
+  midpts <- apply(aprxbounds, 2, function(w)pixel2xy(w,gridsize))
+  # Use their x coordinates to find nearby points on the line determined by u and v.
+  # These will be the approximate centers of the sides of the rectangle which bound
+  # the tissue. Sort to facilitate moving them outward to find exact centers.
+  x <- sort(midpts[1,])
+  y <- m*x + b
+  # Find the endpoints of a line segment of the given height, perpendicular to the 
+  # line determined by u and v, and centered at the origin. Translations of these
+  # points will form the corners of the rectangle.
+  # 1. Form a unit vector, normal to v-u in the counterclockwise sense, i.e., as
+  # if v-u were rotated pi/2 counterclockwise and normalized.
+  unit <- c(-1,1)*(v-u)[2:1]/sqrt(sum((v-u)^2))
+  # 2. Form the endpoints
+  ep1 <- height*unit/2
+  ep2 <- -height*unit/2
+  # Move the centers outward until line segments of the given height, perpendicular
+  # to the line determined by u and v, does not intersect tissue.
+  
+}
